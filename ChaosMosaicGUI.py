d70a0b51b3ac93febd3a560783e5d65e90a1ec1a
debson <debsonosbed@gmail.com>
2020-11-18 19:17:11 +0000

    gui

--- ChaosMosaicGUI.py
+++ ChaosMosaicGUI.py
@@ -12,35 +12,61 @@ class ChaosMosaicGui(Frame):
         self._root.title("Texture Synthesis Chaos Mosaic")
         self._root.resizable(width=True, height=True)
 
+        Frame.__init__(self, root)
+        self.grid(row=0, column=0, sticky=tk.E+tk.W+tk.N+tk.S)
+
+        self._root.rowconfigure(0, weight=1)
+        self._root.columnconfigure(0, weight=1)
+
+
         # Left side panel - options go here
-        self._optionsFrame = Frame(self._root)
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
+        self._leftPanel = Frame(self)
+        self._leftPanel.grid(row=0, column=0, sticky=tk.N + tk.S + tk.E)
 
 
 
-        # Output image file panel
-        self._outputImageFrame = Frame(self._root)
-        self._outputImageFrame.grid(row=0, column=0, sticky=tk.E+tk.W+tk.N+tk.S)
+        # Right panel configuration
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
 
-        self._outputImageFrame.rowconfigure(0, weight=1)
-        self._outputImageFrame.columnconfigure(0, weight=1)
+        self._rightPanel = Frame(self)
+        self._rightPanel.grid(row=0, column=1, sticky=tk.N + tk.S + tk.E)
 
-        self._original = Image.open("out/out.jpg")
-        self._image = ImageTk.PhotoImage(self._original )
-        self._display = Canvas(self._outputImageFrame, bd=0, highlightthickness=0)
-        self._display.create_image(0, 0, image=self._image, anchor=tk.NW, tags="IMG")
+        self._rightPanel.rowconfigure(1, weight=1)
+        self._rightPanel.columnconfigure(0, weight=1)
 
-        self._display.grid(row=0, column=0, sticky=tk.E+tk.W+tk.N+tk.S)
-        self._outputImageFrame.pack(fill=tk.BOTH, expand=1)
+        self._outputInfoPanel = Frame(self._rightPanel)
+        self._outputInfoPanel.grid(row=0, column=0, sticky=tk.N + tk.S)
 
-        self._display.bind("<Configure>", self.resize)
+        self._outputImageLabel = Label(self._outputInfoPanel, text="Synthesized Texture")
+        self._outputImageLabel.grid(row=0, column=0, sticky=tk.E+tk.W+tk.N+tk.S)
 
-    def resize(self, event):
+        self._outputImagePanel = Frame(self._rightPanel)
+        self._outputImagePanel.grid(row=1, column=0, sticky=tk.N + tk.S + tk.E)
+
+        self._outputImagePanel.rowconfigure(0, weight=1)
+        self._outputImagePanel.columnconfigure(0, weight=1)
+
+        self._outputImage = Image.open("out/out.jpg")
+        self._outputImageTk = ImageTk.PhotoImage(self._outputImage)
+        self._outputImageCanvas = Canvas(self._outputImagePanel, bd=0, highlightthickness=0)
+        self._outputImageCanvas.create_image(0, 0, image=self._outputImageTk, anchor=tk.NW, tags="IMG")
+        self._outputImageCanvas.grid(row=0, column=0, sticky=tk.E + tk.W + tk.N + tk.S)
+
+        self.pack(fill=tk.BOTH, expand=1)
+
+        self._rightPanel.bind("<Configure>", self.resize2)
+
+    def resize2(self, event):
         size = (event.width, event.height)
 
-        resized = self._original.resize(size, Image.ANTIALIAS)
-        self._image = ImageTk.PhotoImage(resized)
-        self._display.delete("IMG")
-        self._display.create_image(0, 0, image=self._image, anchor=tk.NW, tags="IMG")
+        resized = self._outputImage.resize(size, Image.ANTIALIAS)
+        self._outputImageTk = ImageTk.PhotoImage(resized)
+        self._outputImageCanvas.delete("IMG")
+        self._outputImageCanvas.create_image(0, 0, image=self._outputImageTk, anchor=tk.NW, tags="IMG")
 
     def _uploadImage(self):
         # choose image from file selector and upload to input label
